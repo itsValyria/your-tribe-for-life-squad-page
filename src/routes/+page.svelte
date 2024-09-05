7eb43988bc37d26510905a69d9427b69accd3459
Valyria <76444716+itsValyria@users.noreply.github.com>
2024-09-05 14:42:20 +0200

    import styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<h2>Jesse sucks</h2>
-<h3>Tom is nogal gay</h3>
+<script>
+  import '../styles/global.css';
+</script>
+
+<main>
+
+</main>
+
+<style>
+
+</style>
