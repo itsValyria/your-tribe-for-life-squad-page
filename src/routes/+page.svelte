0711dabc3e7d1b59625f57460328a75c8963bbab
Valyria <76444716+itsValyria@users.noreply.github.com>
2024-09-05 15:55:52 +0200

    almost home page hehe

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,51 @@
 <script>
-  import '../styles/global.css';
+  import '../global.css';
 </script>
 
 <main>
-
+  <section>
+    <h1>Our team of experts</h1>
+    <p>We are enthousiastic students who are studying Front-End Development and Design at the Amsterdam University of Applied Sciences.</p>
+  </section>
+  <div>
+    <img src="/lesley.jpg" alt="Lesley">
+    <p>F</p>
+    <img src="/akiko.jpg" alt="Akiko">
+    <p>D</p>
+    <img src="/rukiya.jpg" alt="Rukiya">
+    <p>N</p>
+    <img src="/zainab.jpg" alt="Zainab">
+    <p>D</p>
+    <img src="/eva.jpg" alt="Eef">
+  </div>
 </main>
 
 <style>
+  main {
+    padding: 2rem;
+    background-color: var(--blueberry);
+    color: var(--turquoise);
+  }
 
+  img {
+    width: 6.6rem;
+  }
+
+  div {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 0.1rem 0;
+  }
+
+  div p {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-weight: bold;
+    font-size: 1.2rem;
+  }
+
+  div img {
+    margin: 0 auto;
+  }
 </style>
